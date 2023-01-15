7123fb0d2505013f35a071fa8f06e74e0ee99137
Brooke <brooke-burris@outlook.com>
2023-01-14 21:25:37 -0600

    commit tree 1872783c5fb60f8142e6644619fb9363f49b60ed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+ cats.push("Ralph")
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+}
+function appendCat(name) {
+    const copyCats = [...cats, name]
+    return copyCats
+} 
+function prependCat(name) {
+    const copyCatsTwo = [name, ...cats]
+    return copyCatsTwo
+}
+function removeLastCat(name) {
+    const removeCats = cats.slice(0,-1)
+    return removeCats
+}
+function removeFirstCat(name) {
+   const removeOneCat = cats.slice(1)
+   return removeOneCat
+}
+
+    
